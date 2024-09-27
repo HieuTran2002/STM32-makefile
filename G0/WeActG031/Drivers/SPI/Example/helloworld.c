cb7311ffea8dc40f8e3969519152f1cceb75f415
HieuTran2002 <hieutran2002@gmail.com>
2024-09-27 23:06:55 +0700

    update f1

--- G0/WeActG031/Drivers/SPI/Example/helloworld.c
+++ G0/WeActG031/Drivers/SPI/Example/helloworld.c
@@ -1,65 +1,82 @@
-#include "stm32f411xx.h"
-#include "stm32f411xx_clock.h"
+#include "stm32g031_gpio_driver.h"
+#include "stm32g031_spi_driver.h"
+#include "stm32g031xx.h"
+#include "stm32g031_clock.h"
 #include <stdint.h>
 
-void SPI_Transmit(SPI_Type *pSPIx, uint8_t *pTxBuffer, uint32_t length) {
-    while (length > 0) {
-        // Wait until TXE is set (Transmit buffer empty)
-        while (!(pSPIx->SR & (1 << 1)));
 
-        // Send data
+void Clock_Config(void){
+    RCC->CR |= 1;
+    while(!(RCC->CR & (1 << 1)));
 
-        if (pSPIx->CR1 & (1 << 11)){
-            pSPIx->DR = *(uint16_t *)pTxBuffer;
-            pTxBuffer++;
-            length--;
-        }
-        else{
-            pSPIx->DR = *pTxBuffer;
-            pTxBuffer++;
-            length--;
-
-        }
-    }
-
-    // Wait until BSY flag is cleared (Transmission completed)
-    while (pSPIx->SR & (1 << 7));
+    RCC->CFGR &= ~(1);
 }
 
-/* Enable GPIOB and SPI2 Peripheral Clocks */
-void SPI2_GPIO_Init(void){
-    /* Enable GPIO Clock */
+void GPIO_Config(void){
+    RCC->IOPENR |= 0b111;
 
-    /* 
-     * PC2  : SPI2_MISO
-     * PC3  : SPI2_MOSI
-     * PB10 : SPI2_SCK
-     */
+    GPIO_Handle_Type GPIO_Handle;
 
-    GPIOC->MODER    &= ~(1 << 4 | 1 << 6);
-    GPIOC->MODER    |= 2 << 4 | 2 << 6;
-
-    GPIOC->AFR[0]   &= ~(0xF << 8 | 0xF << 12);    
-    GPIOC->AFR[0]   |= 5 << 8;
-    GPIOC->AFR[0]   |= 5 << 12;
-
-    GPIOC->PUPDR |= 0b11 << 4 | 0b11 << 6;
-
-    GPIOB->MODER    &= ~(1 << 20);
-    GPIOB->MODER    |= 2 << 20;
-    GPIOB->AFR[1]   |= 5 << 8;  
-
-    GPIOB->PUPDR |= 0b11 << 20;
-
-
+    GPIO_Handle.pGPIO                           = GPIOA;
+    GPIO_Handle.GPIO_Config.PinNumber           = 4;
+    GPIO_Handle.GPIO_Config.PinMode             = GPIO_MODER_OUTPUT;
+    GPIO_Init(&GPIO_Handle);
 }
 
-/* Initialize SPI2 Peripheral */
+void SPI_Pin_Config(void){
+    /* PA3 SPI2_MISO
+     * PB7 SPI2_MOSI
+     * PA0 SPI2_SCK
+     * */
+
+    /* Enable GPIOA+B */
+    RCC->IOPENR |= 3;
+
+    GPIO_Handle_Type SPI_GPIO_Handle;
+    SPI_GPIO_Handle.pGPIO = GPIOA;
+
+    SPI_GPIO_Handle.GPIO_Config.PinMode         = GPIO_MODER_ALTFNC;
+    SPI_GPIO_Handle.GPIO_Config.Pull            = GPIO_PUPDR_NONE;
+    SPI_GPIO_Handle.GPIO_Config.OType           = GPIO_OTYPE_PP;
+    SPI_GPIO_Handle.GPIO_Config.Speed           = GPIO_OSPEED_MEDIUM;
+
+
+    SPI_GPIO_Handle.GPIO_Config.PinNumber       = 0;
+    SPI_GPIO_Handle.GPIO_Config.AltFunc         = 0;
+    GPIO_Init(&SPI_GPIO_Handle);
+
+    SPI_GPIO_Handle.GPIO_Config.PinNumber       = 3;
+    SPI_GPIO_Handle.GPIO_Config.AltFunc         = 0;
+    GPIO_Init(&SPI_GPIO_Handle);
+
+    SPI_GPIO_Handle.pGPIO                       = GPIOB;
+    SPI_GPIO_Handle.GPIO_Config.PinNumber       = 7;
+    SPI_GPIO_Handle.GPIO_Config.AltFunc         = 1;
+    GPIO_Init(&SPI_GPIO_Handle);
+}
+
+void SPI_Config(void){
+    RCC->APBENR1 |= (1 << 14);
+
+    SPI_Handle_Type SPI_Handle;
+
+    SPI_Handle.pSPI                             = SPI2;
+    SPI_Handle.SPI_Config.DeviceMode            = SPI_DEVICE_MODE_MASTER;
+    SPI_Handle.SPI_Config.CPOL                  = 0;
+    SPI_Handle.SPI_Config.CPHA                  = 0;
+    SPI_Handle.SPI_Config.BusConfig             = SPI_BUS_CONFIG_FD;
+    SPI_Handle.SPI_Config.SclkSpeed             = 7; 
+    SPI_Handle.SPI_Config.DFF                   = SPI_DFF_8BITS;
+    SPI_Handle.SPI_Config.SSM                   = 1;
+
+    SPI_Init(&SPI_Handle);
+}
+
 void SPI2_Init(void){
     uint32_t temp = 0;
 
     /* Enable SPI2 Clock */
-    RCC->APB1ENR |= (1 << 14);  // Enable SPI2 clock
+    RCC->APBENR1 |= (1 << 14);  // Enable SPI2 clock
 
     /* Configure SPI2 as Master, 8-bit data, SSM enabled, CPOL/CPHA low */
     temp = 0;  // Clear control register
@@ -68,7 +85,7 @@ void SPI2_Init(void){
 
     temp |= (7 << 3); /* Baudrate */
 
-    temp &= ~(1 << 11); /* DFF 0 = 8 bits, 1 = 16 bits */
+    temp &= ~(1 << 11); /* CRCL = 8 bits */ 
 
     temp |= (1 << 9);  /* SSM = 1 (Software Slave Management) */ 
 
@@ -77,27 +94,21 @@ void SPI2_Init(void){
     temp |= (1 << 6);  // SPE = 1 (Enable SPI)
 
     SPI2->CR1 |= temp;
+    SPI2->CR2 |= 0b0111 << 8;
 }
 
 
-/* Main function */
-int main(void){
-    RCC->CR |= 1;
-
-    RCC->AHB1ENR |= 0b1111;
-
-    GPIOA->MODER |= 1 << 10;
-
-    SPI2_GPIO_Init();
+int main(void)
+{
+    // Clock_Config();
+    // GPIO_Config();
+    SPI_Pin_Config();
     SPI2_Init();
 
-    /* Send data over SPI2 */
     char string[] = "hello world\n";
 
-    while (1){
-        GPIOA->ODR |= (1 << 5);
-        SPI_Transmit(SPI2, (uint8_t *)string, 12);
-        GPIOA->ODR &= ~(1 << 5);
+    while (1) {
+        SPI_SendData(SPI2, (uint8_t *)string, sizeof(string) - 1);
         delay_ms(100);
     }
 }
