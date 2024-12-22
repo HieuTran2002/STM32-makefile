e31412fbf20be66ad5517741397410fde05d6236
HieuTran2002 <hieutran2001@gmail.com>
2024-12-22 20:43:38 +0700

    add .gdbinit

---
+++ G0/WeActG031/Drivers/GPIO/Example/blink.c
@@ -0,0 +1,25 @@
+#include "stdint.h"
+#include "stm32g031_gpio_driver.h"
+#include "stm32g031_clock.h"
+
+void Init_GPIO(){
+    // enable GPIOA
+    RCC->IOPENR |= 0x1;
+
+    GPIO_Handle_Type config;
+    config.pGPIO = GPIOA;
+    config.GPIO_Config.OType = GPIO_OTYPE_PP;
+    config.GPIO_Config.PinMode = GPIO_MODER_OUTPUT;
+    config.GPIO_Config.PinNumber = 4;
+
+    GPIO_Init(&config);
+}
+
+int main(){
+    Init_GPIO();
+
+    for (;;) {
+        GPIOA->ODR ^= (1 << 4);
+        delay_ms(100);
+    }
+}
